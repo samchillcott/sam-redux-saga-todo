1f76ee1a33b5c78e3190c93a2214fd6d20de6914
Sam Chillcott <sam@secret-source.eu>
2020-12-21 09:08:47 +0000

    editTodo action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,9 @@
 //Action Creator
-export const addTodo = (value) => {
+export const addTodo = (obj) => {
     // return an action
     return {
         type: 'ADD_TODO',
-        payload: value
+        payload: obj
     }
 }
 
@@ -18,5 +18,12 @@ export const removeTodo = (obj) => {
     return {
         type: 'REMOVE_TODO',
         payload: obj
+    }
+}
+
+export const editTodo = (value, obj) => {
+    return {
+        type: 'EDIT_TODO',
+        payload: {value: value.target.value, obj: obj}
     }
 }
