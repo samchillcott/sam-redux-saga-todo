4b7178859a360c71d18bb91bf35c2ca91178099f
Sam Chillcott <samchillcott@gmail.com>
2020-12-31 09:27:22 +0000

    sagas functioning

--- src/redux/Sagas/todosSagas.js
+++ src/redux/Sagas/todosSagas.js
@@ -1,31 +1,13 @@
 import {call, put, takeLatest} from 'redux-saga/effects';
 import { fetchTodos } from '../../api';
-import { downloadTodos } from '../actions';
-
-export function* todosSagas(){
-    yield takeLatest(downloadTodos, handleDownloadTodos)
-}
 
 function* handleDownloadTodos() {
     console.log("handleDownloadTodos worker saga fired");
     const response = yield call(fetchTodos);
     console.log({response});
-    yield put(response);
-    // return response;
-    // yield put(loadTodos(newTodos));
-    // yield put(newTodos);
+    yield put({type: 'USE_DOWNLOAD_TODOS', payload: response})
 }
 
-
-// export function* todosSagas(){
-//     console.log("todosSagas");
-//     yield takeLatest(addTodo, handleAddTodo)
-// }
-
-// function* handleAddTodo({payload: todoText}) {
-//     console.log("handleAddTodo");
-//     const response = yield call(saveTodos, todoText);
-//     // console.log({response});
-//     const newTodos = response.data;
-//     // yield put(loadTodos(newTodos));
-// }
+export function* todosSagas(){
+    yield takeLatest('DOWNLOAD_TODOS', handleDownloadTodos)
+}
